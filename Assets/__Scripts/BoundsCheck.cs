d6c88cf4b5893edf17cca4c0029ee9e704d2f7ea
highesttt <91629626+highesttt@users.noreply.github.com>
2024-10-06 16:59:47 -0500

    feat(code): 🎸 Added bound checker

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoundsCheck : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class BoundsCheck : MonoBehaviour {
+    public enum eType {
+        center,
+        inset,
+        outset
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [Header("Inscribed")]
+    public eType boundsType = eType.center;
+    public float radius = 1f;
+
+    [Header("Dynamic")]
+    public float camWidth;
+    public float camHeight;
+
+    void Awake() {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+
+    void LateUpdate() {
+
+        float checkRadius = 0;
+        if (boundsType == eType.inset) {
+            checkRadius = -radius;
+        } else if (boundsType == eType.outset) {
+            checkRadius = radius;
+        }
+
+
+        Vector3 pos = transform.position;
+        if (pos.x > camWidth + checkRadius) {
+            pos.x = camWidth + checkRadius;
+        }
+
+        if (pos.x < -camWidth - checkRadius) {
+            pos.x = -camWidth - checkRadius;
+        }
+
+        if (pos.y > camHeight + checkRadius) {
+            pos.y = camHeight + checkRadius;
+        }
+
+        if (pos.y < -camHeight - checkRadius) {
+            pos.y = -camHeight - checkRadius;
+        }
+
+        transform.position = pos;
     }
 }
