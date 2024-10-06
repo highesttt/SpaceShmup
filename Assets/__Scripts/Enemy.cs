817bf4334d8c461c59ff75cd16c620f792bb0fd7
highesttt <91629626+highesttt@users.noreply.github.com>
2024-10-06 18:05:00 -0500

    feat(code): 🎸 Added projectiles that kill enemies
    projectiles come out of the hero ship when pressing space

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -38,4 +38,14 @@ public class Enemy : MonoBehaviour
         tempPos.y -= speed * Time.deltaTime;
         pos = tempPos;
     }
+
+    void OnCollisionEnter(Collision coll) {
+        GameObject otherGO = coll.gameObject;
+        if (otherGO.GetComponent<ProjectileHero>() != null) {
+            Destroy(otherGO);
+            Destroy(gameObject);
+        } else {
+            print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+        }
+    }
 }
--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -14,6 +14,8 @@ public class Hero : MonoBehaviour
     public float speed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 40;
 
     [Header("Dynamic")] [Range(0, 4)]
     private float _shieldLevel = 1;
@@ -24,8 +26,7 @@ public class Hero : MonoBehaviour
     void Awake() {
         if (S == null) {
             S = this;
-        }
-        else {
+        } else {
             Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
         }
     }
@@ -41,6 +42,17 @@ public class Hero : MonoBehaviour
         transform.position = pos;
 
         transform.rotation = Quaternion.Euler(vAxis * pitchMult, hAxis * rollMult, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            TempFire();
+        }
+    }
+
+    void TempFire() {
+        GameObject projGO = Instantiate<GameObject>(projectilePrefab);
+        projGO.transform.position = transform.position;
+        Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+        rigidB.velocity = Vector3.up * projectileSpeed;
     }
 
     void OnTriggerEnter(Collider other) {
