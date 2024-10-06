da2b6ea23cd529d9bb8b5a361bede52a26ff4777
highesttt <91629626+highesttt@users.noreply.github.com>
2024-10-06 17:43:00 -0500

    feat(code): 🎸 Added random spawning enemies

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoundsCheck : MonoBehaviour {
+
+    [System.Flags]
+    public enum eScreenLocs {
+        onScreen = 0,
+        offRight = 1,
+        offLeft = 2,
+        offUp = 4,
+        offDown = 8
+    }
+
     public enum eType {
         center,
         inset,
@@ -12,8 +22,10 @@ public class BoundsCheck : MonoBehaviour {
     [Header("Inscribed")]
     public eType boundsType = eType.center;
     public float radius = 1f;
+    public bool keepOnScreen = true;
 
     [Header("Dynamic")]
+    public eScreenLocs screenLocs = eScreenLocs.onScreen;
     public float camWidth;
     public float camHeight;
 
@@ -33,22 +45,45 @@ public class BoundsCheck : MonoBehaviour {
 
 
         Vector3 pos = transform.position;
+        screenLocs = eScreenLocs.onScreen;
         if (pos.x > camWidth + checkRadius) {
             pos.x = camWidth + checkRadius;
+            screenLocs |= eScreenLocs.offRight;
         }
 
         if (pos.x < -camWidth - checkRadius) {
             pos.x = -camWidth - checkRadius;
+            screenLocs |= eScreenLocs.offLeft;
         }
 
         if (pos.y > camHeight + checkRadius) {
             pos.y = camHeight + checkRadius;
+            screenLocs |= eScreenLocs.offUp;
         }
 
         if (pos.y < -camHeight - checkRadius) {
             pos.y = -camHeight - checkRadius;
+            screenLocs |= eScreenLocs.offDown;
         }
 
+        if (keepOnScreen && !isOnScreen) {
         transform.position = pos;
+            screenLocs = eScreenLocs.onScreen;
+        }
+    }
+
+    public bool isOnScreen {
+        get {
+            return screenLocs == eScreenLocs.onScreen;
+        }
+    }
+
+    public bool LocIs(eScreenLocs loc) {
+
+        if (loc == eScreenLocs.onScreen) {
+            return isOnScreen;
+        }
+
+        return (screenLocs & loc) == loc;
     }
 }
