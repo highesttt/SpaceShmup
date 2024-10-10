4e0b43be97e58603ef05020f1f3c3bc457b8ed31
highesttt <killian1.lelong@epitech.eu>
2024-10-10 18:24:08 -0500

    feat(code): 🎸 Added enemy 3

---
+++ Assets/__Scripts/Utils.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class Utils : MonoBehaviour {
+    public static Vector3 Bezier(float u, params Vector3[] points) {
+        Vector3[,] vArr = new Vector3[points.Length, points.Length];
+
+        int r = points.Length - 1;
+        for (int c = 0; c <= r; c++) {
+            vArr[r, c] = points[c];
+        }
+
+        for (r-- ; r >= 0; r--) {
+            for (int c = 0; c <= r; c++) {
+                vArr[r, c] = Vector3.LerpUnclamped(vArr[r + 1, c], vArr[r + 1, c + 1], u);
+            }
+        }
+
+        return vArr[0, 0];
+    }
+}
